9ae7196a5ab2aa994f4247e073a7e55a148f156c
ajayjamage3 <ajayjamage3@gmail.com>
2023-02-12 20:39:52 +0530

    Portfolio

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ var typing=new Typed(".text", {
     backSpeed: 100  ,
     loop: true,
 });
+
+document.querySelector("#nav-toggle").addEventListener("click",()=>{
+    if( document.querySelector(".nav__menu").style.display == "block"){
+        document.querySelector(".nav__menu").style.display = "none"
+    }else{
+        document.querySelector(".nav__menu").style.display = "block"
+    }
+   
+})
+document.querySelector("#nav-toggle").addEventListener("mouseover",()=>{
+
+        document.querySelector(".nav__menu").style.display = "block"
+    
+})
+
+document.querySelector("#nav-toggle").addEventListener("mouseout",()=>{
+
+    document.querySelector(".nav__menu").style.display = "none"
+
+})
